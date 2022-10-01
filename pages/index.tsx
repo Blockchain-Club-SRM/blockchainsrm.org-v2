99d7a6e73e5e45ddf304637c71a87ae3e42cd9cb
Exion <70258985+HarashnabinRoy@users.noreply.github.com>
2022-10-01 17:22:55 +0530

    Update Index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,13 @@
 import type { NextPage } from "next";
 import { Layout } from "../components/shared";
 import { Landing } from "../components/index";
+import  AboutUs  from "../components/index/about";
 
 const Home: NextPage = () => {
   return (
     <Layout>
       <Landing />
+      <AboutUs />
     </Layout>
   );
 };
