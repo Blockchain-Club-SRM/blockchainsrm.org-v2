3c1be46bb66b2ef0182ca7581f3d8e28b0dde607
mdfarhaan <farhaanm110@gmail.com>
2022-10-23 16:54:05 +0530

    :sparkles: Add Events Section

--- components/index/index.ts
+++ components/index/index.ts
@@ -1,1 +1,2 @@
 export { default as Landing } from "./landing";
+export { default as Events } from "./events";
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,18 @@
 import type { NextPage } from "next";
 import { Layout } from "../components/shared";
-import { Landing } from "../components/index";
+import { Landing, Events } from "../components/index";
 
-import  AboutUs  from "../components/index/about";
+import AboutUs from "../components/index/about";
 
 import Dummy from "../components/shared/dummy";
-
 
 const Home: NextPage = () => {
   return (
     <Layout>
       <Landing />
+      <Events />
       <AboutUs />
-      <Dummy/>
+      <Dummy />
     </Layout>
   );
 };
