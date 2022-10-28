8a5fadcc1755997e145659554015898cc691c478
agarwalviraj <viraj.ag007@gmail.com>
2022-10-28 19:40:34 +0530

    fix carousel

--- components/index/carousel.tsx
+++ components/index/carousel.tsx
@@ -1,11 +1,8 @@
 import { useState, useEffect } from "react";
 import { config } from "react-spring";
 import dynamic from "next/dynamic";
-const Carousel = dynamic(() => import("react-spring-3d-carousel"), {
-  ssr: false,
-});
 
-export default function CarouselSection(props) {
+export default function CarouselSection(props: any) {
   const table = props.cards.map((element: any, index: number) => {
     return { ...element, onClick: () => setGoToSlide(index) };
   });
@@ -19,6 +16,11 @@ export default function CarouselSection(props) {
     setOffsetRadius(props.offset);
     setShowArrows(props.showArrows);
   }, [props.offset, props.showArrows]);
+
+  const Carousel: any = dynamic(() => import("react-spring-3d-carousel"), {
+    ssr: false,
+    loading: () => <div>loading...</div>,
+  });
 
   return (
     <div
---
+++ types.d.ts
@@ -0,0 +1,1 @@
+declare module "react-spring-3d-carousel";
