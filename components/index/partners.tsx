0043cdf091ee954a7e580d46dbd7943ce7350e21
siddhardha123 <siddhardha123123@gmail.com>
2023-02-06 20:37:53 +0530

    added partners

--- components/index/partners.tsx
+++ components/index/partners.tsx
@@ -1,24 +1,28 @@
 import React from "react";
-import p from "../../public/assets/partner1.png"
+import p1 from '../../public/partner1.png'
+import p2 from '../../public/partner2.png'
+import p3 from '../../public/partner3.png'
+import p4 from '../../public/partner4.png'
+const data = [
+   {
+      id : 1,
+      img : p1
+   },
+   {
+      id : 2,
+      img : p2
+   },
+   {
+      id : 3,
+      img : p3
+   },
+   {
+      id : 4,
+      img : p4
+   },
+]
 const Partners = () => {
-  const data = [
-    {
-      id: 1,
-      img: "https://drive.google.com/uc?export=view&id=1MDmUn7HhOGM9cDCY0DqCOyfbh8Ku9IRv",
-    },
-    {
-      id: 2,
-      img: "https://drive.google.com/uc?export=view&id=1Z87FYvvzl3Zvvrpr9iR5k87iJpm9zisb",
-    },
-    {
-      id: 3,
-      img: "https://drive.google.com/uc?export=view&id=1lsJjmuog5fFVAif7OlOoO84DNdyo0YyC",
-    },
-    {
-      id: 4,
-      img: "https://drive.google.com/uc?export=view&id=1vXnPs1KToH7gqY1-6loRBJ_We_Y6yA9J",
-    },
-  ];
+
   return (
     <div className="my-14 flex justify-center flex-col gap-12 text-white md:max-w-[100%] mx-auto relative">
       <h6 className="text-transparent bg-clip-text bg-subheading-gradient max-w-max self-center font-normal">
@@ -27,11 +31,12 @@ const Partners = () => {
 
       <h1 className="font-bold self-center">Our Partners</h1>
       <div className="md:flex ">
-        {data.map((data, index) => (
-             <div className="mx-12 md:my-12 ">
-                   <img  className="mx-auto" src={data.img} alt="hello" />
+        {data.map((data)=>(
+            <div className="mx-12 md:my-12 " >
+                   <img  className="mx-auto" src={data.img.src} alt="img" />
              </div>
         ))}
+        
       </div>
     </div>
   );
