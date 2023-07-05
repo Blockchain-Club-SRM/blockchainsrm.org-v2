8dcf5d6ff6069d25bf012216c44195ad2a922b73
Ronin@73 <raghuvanshiatul627@gmail.com>
2023-07-05 19:46:23 +0530

    reducing the spacing between the hero and about seciton more

--- components/index/about.tsx
+++ components/index/about.tsx
@@ -7,15 +7,14 @@ const AboutUs = () => {
   return (
     <div
       id="about-us"
-      className="mt-2 flex justify-center flex-col gap-6 text-white md:max-w-[80%] mx-auto relative"
+      className="mt-0 flex justify-center flex-col gap-10 text-white md:max-w-[80%] mx-auto relative"
     >
       <h6 className="text-transparent bg-clip-text bg-subheading-gradient max-w-max self-center font-normal">
         Everything About our Club
       </h6>
 
       <h1 className="font-bold self-center">About Us</h1>
-
-      <h4 className="flex justify-center mt-2 text-center bg-about-gradient rounded-lg p-8 font-light leading-8">
+      <h4 className="flex justify-center mt-1 text-center bg-about-gradient rounded-lg p-6 font-light leading-7">
         Blockchain Club SRM is the pioneer student-led club around concepts of
         Web3 and distributed consensus. We are a club of 40+ strong members with
         expertise in various domains and technologies. Blockchain Club SRM is a
@@ -29,9 +28,9 @@ const AboutUs = () => {
         <img
           src={horz.src}
           alt=""
-          className="px-6 z-30 max-h-32 hidden lg:block col-start-1 col-end-3 row-span-2"
+          className="px-4 z-30 max-h-32 hidden lg:block col-start-1 col-end-3 row-span-2"
         />
-        <h4 className="lg:-mt-6 ml-0 lg:-ml-[5%] xl:-ml-[15%] font-light leading-8 max-w-3xl col-start-2 row-span-2 col-span-2">
+        <h4 className="lg:-mt-6 ml-0 lg:-ml-[5%] xl:-ml-[15%] font-light leading-7 max-w-3xl col-start-2 row-span-2 col-span-2">
           Empowering Blockchain Professionals And Enthusiasts, And Instilling
           Knowledge To Be Trailblazers In The Industry. Providing Support And
           Guidance And Educating The Students With Latest Advancements In This
@@ -46,9 +45,9 @@ const AboutUs = () => {
         <img
           src={horz.src}
           alt=""
-          className="px-6 z-30 max-h-32 hidden lg:block col-start-1 col-end-3 row-span-2"
+          className="px-4 z-30 max-h-32 hidden lg:block col-start-1 col-end-3 row-span-2"
         />
-        <h4 className="lg:-mt-6 ml-0 lg:-ml-[5%] xl:-ml-[15%] font-light leading-8 max-w-3xl col-start-2 row-span-2 col-span-2">
+        <h4 className="lg:-mt-6 ml-0 lg:-ml-[5%] xl:-ml-[15%] font-light leading-7 max-w-3xl col-start-2 row-span-2 col-span-2">
           Providing Students With Opportunities And Projects On A Regular Basis.
           Educating Them On New Terms In Blockchain And Encouraging Them To
           Participate In Events, Hackathons, Ideathons And Conferences.
@@ -58,12 +57,17 @@ const AboutUs = () => {
       <img
         src={verticalLine.src}
         alt=""
-        className="absolute z-20 h-3/5 top-1/3 mt-2 px-6 hidden lg:block"
+        className="absolute z-20 h-3/5 top-1/3 mt-0 px-6 hidden lg:block"
       />
+      {/* <img
+        src={verticalLine.src}
+        alt=""
+        className="absolute z-20 h-3/5 top-1/3 mt-0 px-2 hidden lg:block"
+      /> */}
       <img
         src="assets/mission-hand.png"
         alt=""
-        className="absolute -right-60 scale-75 -z-10 bottom-[-20%]"
+        className="absolute -right-48 scale-75 -z-10 bottom-[-30%]"
       />
       <img
         src="assets/vision-statue.png"
--- components/index/landing.tsx
+++ components/index/landing.tsx
@@ -5,22 +5,22 @@ import more from "../../public/assets/more_btn.png";
 
 const Landing = () => {
   return (
-    <div className="text-white px-4 pb-0 md:pb-2 flex justify-center items-center md:flex-row flex-col min-h-[100vh]">
-      <div className="grid place-items-center md:place-items-start mt-4 md:mt-0">
-        <p className="font-extralight mb-4 md:mb-6 text-md">
+<div className="text-white px-4 pb-0 md:pb-2 flex justify-center items-center md:flex-row flex-col min-h-[100vh] mt-0 md:mt-[-10px] mb-0 md:mb-[-10px]">
+      <div className="grid place-items-center md:place-items-start mt-2 md:mt-0">
+        <p className="font-extralight mb-1 md:mb-2 text-md">
           WELCOME TO BLOCKCHAIN CLUB SRM
         </p>
-        <h1 className="font-medium lg:text-5xl text-3xl">Changing The World</h1>
-        <h1 className="font-medium lg:text-5xl text-3xl">
+        <h1 className="font-medium lg:text-3xl text-2xl">Changing The World</h1>
+        <h1 className="font-medium lg:text-3xl text-2xl">
           <span className="text-[#00D1FF]">One Block</span> At A Time
         </h1>
-        <p className="font-extralight mt-4 md:mt-6 lg:text-md text-xl lg:w-[60%] md:w-[80%] w-full md:text-start text-center">
+        <p className="font-extralight mt-1 md:mt-2 lg:text-sm text-base lg:w-[60%] md:w-[80%] w-full md:text-start text-center">
           Pioneer student-led blockchain-centered community to foster young and
           inquisitive minds, that believes in implementing concepts of Web3 and
           Decentralization
         </p>
       </div>
-      <div className="md:w-fit w-[70%] md:mt-0 mt-6">
+      <div className="md:w-fit w-[70%] md:mt-0 mt-1">
         <Image src={heroImage} alt="Hero Image" />
       </div>
     </div>
