f02cb9115a9c20b62b5d4cd7d17e3c3c030ce180
ik04 <ishaankhurana123@gmail.com>
2023-07-09 23:04:32 +0530

    feat: fixed responsive navbar onclick events

--- components/shared/menu-item.tsx
+++ components/shared/menu-item.tsx
@@ -5,22 +5,22 @@ const MenuItems = ({ show, active, menu }: any) => {
   return (
     <div>
       <ul
+        onClick={show}
         className={
           active
             ? "flex-col flex items-center fixed inset-0 left-1/4 uppercase gap-8 bg-black/50 backdrop-blur-md justify-center md:hidden "
             : "hidden"
         }
       >
-        <div
-          onClick={show}
-          className="cursor-pointer font-semibold hover:text-primary-pink"
-        >
+        <div className="cursor-pointer font-semibold hover:text-primary-pink">
           x
         </div>
         {menu.map((item: any, index: any) => {
           return (
             <li key={index} className="hover:text-primary-pink">
-              <Link href={item.path}>{item.name}</Link>
+              <Link onClick={show} href={item.path}>
+                {item.name}
+              </Link>
             </li>
           );
         })}
