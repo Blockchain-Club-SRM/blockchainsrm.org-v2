1554625d915f99eee768578dbef51f95ef709fd8
PaleDeath <taseeniqbal.ti@gmail.com>
2024-02-12 01:08:12 +0530

    feat: added footer,prizes section added smooth scroll added layer 2.0 button to menu and changed colour to match main website

---
+++ components/layer/footer.tsx
@@ -0,0 +1,64 @@
+import * as React from "react";
+
+function Footer() {
+  return (
+    <div className="flex flex-col top-[500px]" style={{ backgroundColor: "rgba(11,15,24)" }}>
+      <div className="flex gap-5 mt-0 justify-center items-center px-5 max-w-full">
+        <a href="https://www.instagram.com/blockchainsrm/" target="_blank" rel="noopener noreferrer">
+          <img
+            loading="lazy"
+            src="./instagram.svg"
+            className="h-10 w-10 object-contain"
+            alt="instagram"
+          />
+        </a>
+        <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
+          <img
+            loading="lazy"
+            src="./facebook.svg"
+            className="h-10 w-10 object-contain"
+            alt="facebook"
+          />
+        </a>
+        <a href="https://www.discord.com" target="_blank" rel="noopener noreferrer">
+          <img
+            loading="lazy"
+            src="./discord.svg"
+            className="h-10 w-10 object-contain"
+            alt="discord"
+          />
+        </a>
+        <a href="https://www.telegram.org" target="_blank" rel="noopener noreferrer">
+          <img
+            loading="lazy"
+            src="./telegram.svg"
+            className="h-10 w-10 object-contain"
+            alt="telegram"
+          />
+        </a>
+        <a href="www.linkedin.com/company/blockchain-club-srm/" target="_blank" rel="noopener noreferrer">
+          <img
+            loading="lazy"
+            src="./linkedin.svg"
+            className="h-10 w-10 object-contain"
+            alt="linkedin"
+          />
+        </a>
+        <a href="https://twitter.com/BlockchainSRM" target="_blank" rel="noopener noreferrer">
+          <img
+            loading="lazy"
+            src="./twitter.svg"
+            className="h-10 w-10 object-contain"
+            alt="twitter"
+          />
+        </a>
+      </div>
+      <div className="flex gap-5 justify-between px-5 mt-20 w-full text-3xl tracking-wider text-center text-white max-md:flex-wrap max-md:mt-10 max-md:max-w-full">
+        <div className="flex-auto">Powered by @Devfolio</div>
+        <div className="flex-auto">Crafted by @</div>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
