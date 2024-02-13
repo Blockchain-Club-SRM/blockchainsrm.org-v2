0f117729cec9f8acb6f4fbd4691ac956477cdceb
PaleDeath <taseeniqbal.ti@gmail.com>
2024-02-14 02:36:39 +0530

    fix: fixed header

--- components/shared/header.tsx
+++ components/shared/header.tsx
@@ -17,7 +17,7 @@ const Header = () => {
   const menu = [
     {
       name: (
-        <button className="relative inline-flex items-center justify-center mb-0 p-0.5 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-500 to-pink-500 group-hover:from-purple-500 group-hover:to-pink-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800">
+        <button className="relative inline-flex items-center justify-center p-0.5 mb-2 me-0 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-500 to-pink-500 group-hover:from-purple-500 group-hover:to-pink-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800">
           <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
             LAYER 2.0
           </span>
@@ -48,8 +48,10 @@ const Header = () => {
   ];
 
   return (
+   
     <div
-      className={`w-full text-white flex justify-between p-2 items-center left-0 right-0 z-50 top-0 ${!active && "backdrop-blur-md md:backdrop-blur-none"
+      className={` w-full text-white flex justify-between p-2  items-center left-0 right-0 z-50 top-0 ${
+        !active && " backdrop-blur-md md:backdrop-blur-none"
       }`}
     >
       <div className="flex items-center space-x-3">
@@ -59,10 +61,10 @@ const Header = () => {
         </Link>
       </div>
       <nav>
-        <div className="absolute right-6 top-6 md:hidden cursor-pointer">
+        <div className=" absolute right-6 top-6 md:hidden cursor-pointer">
           <BiMenu onClick={show} size={26} className="cursor-pointer" />
         </div>
-        <ul className="hidden md:flex gap-8 p-5 py-7 lg:w-[60vw] md:justify-center content-center uppercase backdrop-blur-md" style={{ display: 'flex', alignItems: 'center' }}>
+        <ul className="hidden md:flex gap-8 p-5 py-6 lg:w-[50vw] md:justify-center content-center items-center uppercase backdrop-blur-md">
           {menu.map((item, index) => {
             return (
               <li
