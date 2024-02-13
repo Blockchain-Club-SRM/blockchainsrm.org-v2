d9aa6793d290398afca6ccfd35362ad32c8c1701
PaleDeath <taseeniqbal.ti@gmail.com>
2024-02-14 02:39:18 +0530

    fix: final header fix

--- components/shared/header.tsx
+++ components/shared/header.tsx
@@ -17,7 +17,7 @@ const Header = () => {
   const menu = [
     {
       name: (
-        <button className="relative inline-flex items-center justify-center p-0.5 mb-2 me-0 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-500 to-pink-500 group-hover:from-purple-500 group-hover:to-pink-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800">
+        <button className="relative inline-flex items-center justify-center p-0.5 mb-0 me-0 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-500 to-pink-500 group-hover:from-purple-500 group-hover:to-pink-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800">
           <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
             LAYER 2.0
           </span>
