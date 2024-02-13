d50e55cae3b16685d16fc36067beec1c0e74de03
ik04 <ishaankhurana123@gmail.com>
2024-02-13 19:14:33 +0530

    feat: add timer component

--- pages/layer.tsx
+++ pages/layer.tsx
@@ -1,4 +1,4 @@
-"use client";
+import React, { useState, useEffect } from "react";
 import Image from "next/image";
 import LayerHeader from "../components/shared/layerheader";
 import Footer from "../components/layer/footer";
@@ -7,7 +7,6 @@ import ninenine from "../public/99.png";
 import text from "../public/layertext.png";
 import ship from "../public/ship.png";
 import Link from "next/link";
-import { useEffect, useState } from "react";
 import { RiseLoader } from "react-spinners";
 import Judges from "../components/judges/judges";
 import Prizes from "../components/layer/prizes";
@@ -17,6 +16,26 @@ import Faq from "../components/layer/faq";
 
 export default function Layer() {
   const [loading, setLoading] = useState(true);
+  const [days, setDays] = useState<number>(0);
+  const [hours, setHours] = useState<number>(0);
+  const [minutes, setMinutes] = useState<number>(0);
+
+  const calculateTimeUntil = () => {
+    const targetDate = new Date("2024-03-12");
+    const currentDate = new Date();
+    const timeDifference = targetDate.getTime() - currentDate.getTime();
+    const remainingDays = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+    const remainingHours = Math.floor(
+      (timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+    const remainingMinutes = Math.floor(
+      (timeDifference % (1000 * 60 * 60)) / (1000 * 60)
+    );
+    setDays(remainingDays);
+    setHours(remainingHours);
+    setMinutes(remainingMinutes);
+    // console.log(remainingDays, remainingHours, remainingMinutes);
+  };
 
   useEffect(() => {
     const timer = setTimeout(() => {
@@ -24,6 +43,15 @@ export default function Layer() {
     }, 2000);
     return () => clearTimeout(timer);
   }, []);
+
+  useEffect(() => {
+    calculateTimeUntil();
+    const timerInterval = setInterval(() => {
+      calculateTimeUntil();
+    }, 1000);
+    return () => clearInterval(timerInterval);
+  }, []);
+
   return (
     <div className="">
       {loading ? (
@@ -51,12 +79,32 @@ export default function Layer() {
                 src={ship}
                 className=" xl:block absolute  top-[22rem] -left-3 w-[82rem] hidden"
               />
-              <Image
-                loading="eager"
-                alt="99"
-                src={ninenine}
-                className=" self-center mt-[40rem] xl:w-auto w-96 "
-              />
+              <div className="mt-[40rem] pr-9 md:pr-9 xl:mr-0 xl:w-auto w-96 flex justify-center space-x-10">
+                <div className="days flex flex-col items-center space-y-2">
+                  <div className="md:text-center text-start text-2xl mt-4 md:mt-0 md:py-0 md:text-5xl">
+                    <span className="bg-clip-text pl-2 md:pl-0 bg-gradient-to-t from-cyan-700 to-slate-50 text-transparent font-black uppercase font-['Impact']">
+                      {days}
+                    </span>
+                  </div>
+                  <p className="text-white">Days</p>
+                </div>
+                <div className="hours flex flex-col items-center space-y-2">
+                  <div className="md:text-center text-start text-2xl mt-4 md:mt-0 md:py-0 md:text-5xl">
+                    <span className="bg-clip-text pl-2 md:pl-0 bg-gradient-to-t from-cyan-700 to-slate-50 text-transparent font-black uppercase font-['Impact']">
+                      {hours}
+                    </span>
+                  </div>
+                  <p className="text-white">Hours</p>
+                </div>
+                <div className="mins flex flex-col items-center space-y-2">
+                  <div className="md:text-center text-start text-2xl mt-4 md:mt-0 md:py-0 md:text-5xl">
+                    <span className="bg-clip-text pl-2 md:pl-0 bg-gradient-to-t from-cyan-700 to-slate-50 text-transparent font-black uppercase font-['Impact']">
+                      {minutes}
+                    </span>
+                  </div>
+                  <p className="text-white">Minutes</p>
+                </div>{" "}
+              </div>
               <div className="buttons flex flex-col md:flex-row mt-16 self-center gap-14 mb-72 ">
                 <Link href="https://layer-2.devfolio.co/">
                   {" "}
