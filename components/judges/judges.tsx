1b209475af4f6df39824091d7d089e00d804e1c2
MajorTimberWolf <ak0929@srmist.edu.in>
2024-02-25 21:29:23 +0530

    refactor: update team images

--- components/judges/judges.tsx
+++ components/judges/judges.tsx
@@ -2,20 +2,52 @@ import React from "react";
 import Image from "next/image";
 import group1 from "../../public/judgesgroup 1.png";
 import group2 from "../../public/judgesgroup 2.png";
-import pfp from "../../public/ellipse-324@2x.png";
 import type { NextPage } from "next";
 
 const Judges: React.FC = () => {
+  const judges = [ 
+    {
+      name: "Dr. M Lakshmi",
+    pfp: "/HOD.png",
+    description: "Head of Department, DSBS",
+    },
+    {
+      name: "Dr. S. Ganesh Kumar",
+    pfp: "/Ganesh.png",
+    description: "Program Coordinator Blockchain, DSBS",
+    },
+    {
+      name: "Dr. Syed Ismail A",
+    pfp: "/Syed-Ismail.png",
+    description: "Assistant Professor, DSBS",
+    },
+    {
+      name: "Dr. R. Jayaraj",
+    pfp: "/Jayaraj.png",
+    description: "Assistant Professor, DSBS",
+    },
+    {
+      name: "Vedant Utage",
+    pfp: "",
+    description: "President, Blockchain Club SRM", 
+    },
+    {
+      name: "Pratham Bhatnagar",
+    pfp: "",
+    description: "Vice President, Blockchain Club SRM",
+    },
+  ]
+
   return (
     <div id="judges" className="bg-slate-950 flex flex-col min-h-screen">
       <div className="text-center w-full md:my-20 text-5xl mt-4 md:mt-0 md:py-0 md:text-7xl">
         <span className="bg-clip-text pl-2 md:pl-0 bg-gradient-to-t from-cyan-700 to-slate-50 text-transparent font-black uppercase font-['Impact']">
-          Judges
+          Our Team
         </span>
       </div>
 
       <div className="md:ml-0 cards flex flex-col xl:gap-48 mb-20 xl:flex-row xl:flex-wrap xl:px-20 xl:justify-between">
-        {[...Array(6)].map((_, index) => (
+        {judges.map((judge, index) => (
           <div
             key={index}
             className="card1 flex flex-col w-full md:w-1/2 xl:w-1/3 xl:max-w-[350px] mb-10 md:mb-4 xl:mb-0 px-4 relative"
@@ -27,13 +59,15 @@ const Judges: React.FC = () => {
               <Image alt="2" src={group2} />
             </div>
             <div className="absolute w-44 ml-20 mt-14">
-              <Image alt="pfp" src={pfp} />
+              <Image alt="pfp" src={judge.pfp} width={176} height={96} layout="responsive"/>
             </div>
             <div className="flex flex-col mt-80">
               <h1 className="text-white font-bold text-center mt-4">
-                FULL NAME
+                {judge.name}
               </h1>
-              <h2 className="text-white text-center mt-2">Description</h2>
+              <h2 className="text-white text-center mt-2">
+                {judge.description}
+              </h2>
             </div>
           </div>
         ))}
@@ -41,5 +75,4 @@ const Judges: React.FC = () => {
     </div>
   );
 };
-
 export default Judges;
